64e1b40a60cc548e5ce29b5cff1538f954830fcb
Juan Pablo <juampigarcia2014@gmail.com>
2021-04-13 20:15:19 -0300

    fix(setup.py): :fire: reduce setup.py

--- setup.py
+++ setup.py
@@ -7,23 +7,11 @@
 # `python3 setup.py install`
 # --------------------------------------------------
 
-import io
 from setuptools import setup, find_packages
-
-with io.open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
 
 setup(
     name="infram",
     version="0.0.1",
-    description="A small bot for Brave",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     packages=find_packages(),
-    zip_safe=False,
     install_requires=["pyautogui", "pynput"],
-    python_requires="~=3.6",
-    author="",
-    author_email="",
-    url="https://github.com/kbiters/infram",
 )
