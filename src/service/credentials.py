1477f2aa34833b36e70f5cb6830c2f186e714bcb
Giancarlo <gianca9405@gmail.com>
2021-04-15 09:54:27 -0300

    Update

--- src/service/credentials.py
+++ src/service/credentials.py
@@ -2,24 +2,27 @@ from math import sqrt
 
 # SESSION_KEY_ENCRIPTADA = "mseassunioeasomi"
 # SESSION_KEY_NO_ENCRIPTADA = 4819883657198745 <--- la funcion math_operation() devuelve exactamente ese numero
+# VAX_X_ENCRIPTADA = "namgiuee.olmo"
 from src.service.translator import translate
 
-VAR_X = 69425311.7047
+# VAR_X = 69425311.7047
 VAR_Y = 454693252.55989
 
 dictionary = {0: 'l', 1: 'e', 2: 'g', 3: 'u', 4: 'm', 5: 'i', 6: 'n', 7: 'o', 8: 's', 9: 'a'}
 
 
 def check_session():
-    decrypted_hash = decrypt(set_user_hash(), dictionary)
-    if decrypted_hash == math_operation():
+    decrypted_first_hash = decrypt(set_user_first_hash(), dictionary)
+    decrypted_second_hash = decrypt(set_user_second_hash(), dictionary)
+    if decrypted_first_hash == math_operation(decrypted_second_hash):
         print(translate("Authorization code VALIDATED!\n"))
         return True
     else:
         return False
 
 
-def math_operation():
+def math_operation(decrypted_second_hash):
+    VAR_X = float(decrypted_second_hash)
     d = sqrt(VAR_X ** 4 + VAR_Y ** 3)
     return str(round(d, ))
 
@@ -32,8 +35,12 @@ def decrypt(x, d):
     return ''.join(x)
 
 
-def set_user_hash():
+def set_user_first_hash():
     print(translate(
         '\nIf you dont have the code to authorize INFRAM, '
-        'you can get it here: https://discord.gg/z7AmYMMB, in the "AUTHORIZATION" section.'))
-    return list(input(translate("Enter the code that is in the DISCORD group: ")).lower())
+        'you can get it here: https://t.me/joinchat/Pa3q7Hd84uYyMjMx'))
+    return list(input(translate("Enter the code that is in the TELEGRAM group: ")).lower())
+
+
+def set_user_second_hash():
+    return list(input(translate("Enter the second code that is in the TELEGRAM group: ")).lower())
