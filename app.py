3a6b533b7cc32c404a3b7635ec2f689e32f7a7d0
Giancarlo <gianca9405@gmail.com>
2021-04-16 14:31:35 -0300

    refactor

--- app.py
+++ app.py
@@ -1,29 +1,36 @@
+from time import time, sleep
+
 import pyautogui
 
-from autoupdate import auto_update
+from src.operations.functions import check_stop
+from src.operations.mouse import set_use_mouse, move_click_mouse
+from src.operations.windows import start_brave
+from src.service.constants import Config, Message, Brave
+from src.service.translator import select_language, translate
 
 
 def main():
     pyautogui.FAILSAFE = False
-    # Config.LANGUAGE = select_language()
-    # set_use_mouse()
-    auto_update()
+    Config.LANGUAGE = select_language()
+    set_use_mouse()
+
     # if check_session():
 
+    print(translate(Message.WELCOME))
+    initial_time = time()
 
-"""  print(translate(Message.WELCOME))
-  initial_time = time()
+    while True:
+        if check_stop(initial_time):
+            break
 
-  while True:
-      if check_stop(initial_time):
-          break
+        move_click_mouse()
 
-      move_click_mouse()
+        start_brave()
+        sleep(Brave.TIME_TO_REPEAT)
 
-      start_brave()
-      sleep(Brave.TIME_TO_REPEAT)
+        print(f"Finish after: {time() - initial_time}")
 
-  print(f"Finish after: {time() - initial_time}")"""
+
 # else:
 # print(translate(Credentials.ERROR))
 
